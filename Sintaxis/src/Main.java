952f6cbec2035f52f1a50fc92375e27eec167e6b
Angelsan22 <123048340@upq.edu.mx>
2025-01-27 14:38:13 -0600

    Se creo un nuevo documento para hacer la actividad 3, en donde se hizo un programa que calcula el costo tatal a cobrar de un trabajador en donde le pregunta la cantidad de horas trabajadas y a cuanto le pagan la hora, sacando el todal a cobrar

--- Sintaxis/src/Main.java
+++ Sintaxis/src/Main.java
@@ -1,101 +1,17 @@
-//IMPORTACIONES
-//import java.util.Random;
-import java.util.Scanner;
-
-
-//Clase principal
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    //Metodo main (Metodo principal)
+
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printfln("Hello, José Ángel sanchez");
 
-
-//    2. estructura de cadenas y selecciones de sierta cantidad de datos.
-//        System.out.printf("Hello, José Ángel sanchez ");
-//        String cadenas = "Angel " + "Sánchez " + "Linares ";
-//
-//        System.out.print(cadenas);
-//
-//        System.out.println(cadenas.length());
-//
-//        System.out.println(cadenas.substring(2,5));
-//        System.out.println(cadenas.substring(2));
-//        System.out.println(cadenas.substring(0,5));
-
-
-//      3 variables
-
-//        int x=5,x2;
-//        double y=1.25,y2;
-//        String z="204",z1;
-//
-//        //Conversion de cadena a int
-//        x2= Integer.parseInt(z);
-//
-//        //conversion de double a cadena
-//        z1= String.valueOf(y);
-//
-//        //Conversion de int a double
-//        Double asd= Double.valueOf(x);
-//
-//
-//        //Conversion Implicita
-//        int num=12;
-//        double numD=num;
-//        System.out.println("Convercion implicita "+num);
-//
-//        System.out.println(x2);
-//        System.out.println(z1);
-//        System.out.println(asd);
-//
-//
-//        //Creamos un objeto rdn de clase Random
-//        Random rdn=new Random();
-//
-//
-//        //Generamos numeros y los guardamos
-//        int numAleatorio=rdn.nextInt();
-//        Double numDouble=rdn.nextDouble();
-//
-//        System.out.println("Aleatorio entero: "+numAleatorio);
-//        System.out.println("Aleatorio double: "+numDouble);
-
-
-//        /*//4. Solicitud de datos
-//
-//        Scanner scn = new Scanner(System.in);
-//
-//        System.out.println("Introduce cualquier dato");
-//        String dato= scn.nextLine();
-//
-//        System.out.println("Introduce cualquier dato entero");
-//        int datoEntero= scn.nextInt();
-//
-//        System.out.println("Introduce cualquier dato con decimal");
-//        double datoDouble= scn.nextDouble();
-//
-//        System.out.println("Cualquier dato " +dato);
-//        System.out.println("Dato entero " +datoEntero);
-//        System.out.println("Dato decimal " +datoDouble);*/
-
-
-//        5. Boolean, operadores logicos y comparacion
-
-        System.out.println(10 > 9); //true
-        System.out.println(10 == 9); //false
-        System.out.println(10 < 9); //false
-        System.out.println(10 >= 9); //true
-        System.out.println(10 <= 9); //false
-        System.out.println(10 != 9); //true
-
-        int x=3;
-        System.out.println(x<5 && x>10); //false
-        System.out.println(x<5 || x>10); //true
-        System.out.println(!(x< 5 && x>10)); //true
-
-
-
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.printf("i = " + i);
+        }
     }
-
-
 }
