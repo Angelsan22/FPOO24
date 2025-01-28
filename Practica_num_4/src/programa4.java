c5e4cc6b4d7eab8d11ddf147e0631bbf1e4ef5b1
Angelsan22 <123048340@upq.edu.mx>
2025-01-28 17:04:02 -0600

    Se creo el 4to programa en donde pide una frase y de a cuerdo a la frase te dice si es polindromo o no

---
+++ Practica_num_4/src/programa4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class programa4 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero: ");
+        String frase = sc.nextLine();
+
+        String palabraInvertida = new StringBuilder(frase).reverse().toString();
+        if (frase.equalsIgnoreCase(palabraInvertida)) {
+            System.out.println("La palabra: " + palabraInvertida + " es palíndromo");
+        }else {
+            System.out.println("La palabra: " + palabraInvertida + " no es palíndromo");
+        }
+    }
+}
