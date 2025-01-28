582d08a324e5f2a35cd989272457a697fb79a00f
Angelsan22 <123048340@upq.edu.mx>
2025-01-27 22:49:47 -0600

    S e crearon los 6 pregramas que dejo el maestro de actividad, el primero saca el salario del un trabajador en vace a las horas trabajadas y el pago por hora, el segundo pide ingresar el nombre y los apellidos, imprimiendolo 3 veces, el primero en minusculas, el segundo en mayusculas y el tercero solo las iniciales, el tercer programa pide al usuario cuantos numeros quiere y una vez ingresados los numeros los suma dando el total de la suma, el 4to programa nos pide cualquier nombre del usuario y nos dice cuantos caracteres tiene, el 5to programa nos muestra como es que saca el total de gramos que pesa una caja con siertas cantidad de payasos y muñecas, como resultado nos muestra la cantidad de productos, el gramo de los payasos y muñecas y el total de los 2 y pr ultimo el 6to programa nos pide ingresar la frase que quieras y como resultado nos la da invertida

--- Actividad_Prac_3/src/Main.java
+++ Actividad_Prac_3/src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
-
+/*
         Scanner sc = new Scanner(System.in);
 
 
@@ -15,6 +16,107 @@ public class Main {
         Double Total = horas * pago;
 
         System.out.printf("La cantidad a cobrar es: " + Total);
+
+*/
+
+
+/*
+        Scanner sc = new Scanner(System.in);
+
+        String apep, apem, nom;
+
+        System.out.println("Ingrese su apellido paterno");
+        apep = sc.nextLine();
+
+        System.out.println("Ingrese su apellido materno");
+        apem = sc.nextLine();
+
+        System.out.println("Ingrese su nombre");
+        nom = sc.nextLine();
+
+        String apepMinusculas= apep.toLowerCase();
+        String apemMinusculas= apem.toLowerCase();
+        String nomMinusculas= nom.toLowerCase();
+
+        String apepMayusculas= apep.toUpperCase();
+        String apemMayusculas= apem.toUpperCase();
+        String nomMayusculas = nom.toUpperCase();
+
+        String apepNormal= apep.toUpperCase().charAt(0)+"";
+        String apemNormal= apem.toUpperCase().charAt(0)+"";
+        String nomNormal= nom.toUpperCase().charAt(0)+"";
+
+        System.out.println(apepMinusculas  +  apemMinusculas  +   nomMinusculas);
+        System.out.println(apepMayusculas  +  apemMayusculas  +   nomMayusculas);
+        System.out.println(apepNormal+  apemNormal+   nomNormal);
+
+*/
+
+/*        Scanner sc = new Scanner(System.in);
+
+        int x;
+        double suma=0;
+
+        System.out.print("Ingrese un numero entero: ");
+        x = sc.nextInt();
+
+        for (int i = 0; i < x; i++) {
+            System.out.print("Ingrese el numero: "+i +"\n");
+            int y = sc.nextInt();
+
+        }
+        suma = suma + x;
+
+        System.out.println("La suma es: "+suma);
+*/
+
+/*        Scanner sc = new Scanner(System.in);
+
+        String nombre;
+
+        System.out.print("Ingrese su nombre: ");
+        nombre = sc.nextLine();
+
+        String cadenas = nombre.toUpperCase();
+        System.out.print(cadenas+"\n");
+        System.out.print( "tiene: " +cadenas.length() + " caracteres");
+
+*/
+
+/*        Scanner sc = new Scanner(System.in);
+
+        int payaso= 112;
+        int muñecas = 75;
+        int p,m, totalpayaso, totalmuñecas, total, peso;
+
+        System.out.println("Ingrese la cantidad de payasos que lleva la caja: ");
+        p = sc.nextInt();
+
+        System.out.println("Ingrese la cantidad de muñecas que lleva la caja: ");
+        m = sc.nextInt();
+
+        totalpayaso = payaso*p;
+        totalmuñecas= muñecas*m;
+        total = p+m;
+        peso = totalpayaso+totalmuñecas;
+
+        System.out.println("el peso total de peso en los payasos es: " +totalpayaso + "g");
+        System.out.println("El total de peso en las muñecas es: " +totalmuñecas + "g");
+        System.out.println("El total de productos que lleva la caja es: " +total);
+        System.out.println("El peso total del paquete es de: " + peso);
+
+*/
+
+/*        Scanner sc = new Scanner(System.in);
+
+        String frace;
+
+        System.out.print("Introdusca la frase deseada: ");
+        frace = sc.nextLine();
+
+        StringBuilder frase = new StringBuilder(frace);
+        System.out.println("(" + frase + ") = " + frase.reverse());
+
+*/
     }
 }
-
