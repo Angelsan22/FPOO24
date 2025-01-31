343adf22ab904ae43eace43186bdc09f06052fa1
Angelsan22 <123048340@upq.edu.mx>
2025-01-30 21:26:08 -0600

    se creo el  septimo programa en donde  nos pide un numero entero pero que sea impar, en donde crea un arbol tomando como base el numero ingresado
    me confundi de apartado el primer programa lo puse en el septimo y el septimo se introdujo en el primero

--- Practivaca5_POO/src/programa1.java
+++ Practivaca5_POO/src/programa1.java
@@ -1,8 +1,42 @@
+import java.util.*;
+
 public class programa1 {
 
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Introduce la cantidad de * de la base (número impar): ");
+        int base = sc.nextInt();
+        sc.close();
+
+        if (base % 2 == 0) {
+            System.out.println("La base debe ser un número impar.");
+            return;
+        }
+
+        int espacios = base / 2;
+        int estrellas = 1;
+
+        while (estrellas <= base) {
+
+            int i = 0;
+            while (i < espacios) {
+                System.out.print(" ");
+                i++;
+            }
+
+            int j = 0;
+            while (j < estrellas) {
+                System.out.print("*");
+                j++;
+            }
+
+            System.out.println();
+
+            espacios--;
+            estrellas += 2;
+        }
     }
 }
