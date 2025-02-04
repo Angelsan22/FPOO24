924099c6c278ebd02e60cb48aaf2ceca66fb44a7
Angelsan22 <123048340@upq.edu.mx>
2025-02-04 16:27:00 -0600

    El primer programa noc muestra un ciclo for en donde nos muestra todos los multiplos desde 10 hasta 1000 en un aumento de 10 en 10

---
+++ Examen_1er_parcial/src/Ejercicio_1_examen.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ejercicio_1_examen {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        for (int i = 10; i <= 1000; i+=10) {
+            System.out.printf("%d \n", i);
+        }
+    }
+}
