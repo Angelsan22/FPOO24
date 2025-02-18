3efd3a0ad37e78101d66e5d541125ad732a702ec
Angelsan22 <123048340@upq.edu.mx>
2025-02-18 15:51:47 -0600

    prorama de simulacion de una caja popular, en donde muestra opciones como, saldo, depositos, retiros, etc

---
+++ Practica_num_9/src/Caja_popular.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+public class Caja_popular {
+
+
+    String titular;
+    int edad;
+    double saldo = 0;
+    String noCuenta;
+
+    public Caja_popular(String noCuenta, String titular, int edad, double saldo) {
+        String cuentaNoCuenta = noCuenta;
+        String cuentaTitular = titular;
+        edad = edad;
+        double cuentaSaldo = saldo;
+
+    }
+
+    public void consultarSaldo() {
+        System.out.println("Saldo actual: " + saldo);
+    }
+
+    public void ingresarEfectivo(double cantidad) {
+        saldo = saldo + cantidad;
+        System.out.println("Has ingresado: " + cantidad + ". Saldo actual: " + saldo);
+    }
+
+    public void retirarEfectivo(double cantidad) {
+        if (saldo >= cantidad) {
+            saldo = saldo - cantidad;
+            System.out.println("Has retirado: " + cantidad + ". Saldo actual: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente.");
+        }
+    }
+
+    public void depositarOtraCuenta(Caja_popular otraCuenta, double cantidad) {
+        if (saldo >= cantidad) {
+            saldo = saldo - cantidad;
+            otraCuenta.saldo = saldo + cantidad;
+            System.out.println("Has depositado: " + cantidad + " a la cuenta de " + otraCuenta.titular);
+            System.out.println("Tu saldo actual: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente.");
+
+
+        }
+    }
+}
